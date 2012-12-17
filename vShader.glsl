93691c3301a520cb78f94d09896a80eda9f47e76
emccann <emccann@cs.uml.edu>
2012-12-17 13:13:19 -0500

    periodic floor wave amplitude modulation

--- vShader.glsl
+++ vShader.glsl
@@ -16,10 +16,11 @@ uniform bool modulate;
 uniform float time;
 
 float PI = 4.0 * atan(1.0);
-float magnitude1 = 0.1;
-float magnitude2 = 0.2;
+float magnitude1 = 0.02;
+float magnitude2 = 0.03;
 float period1 = 0.5;
 float period2 = 1.0;
+const int cyclelength = 750; //ms
 
 void main() 
 {	
@@ -28,7 +29,13 @@ void main()
 	{		
 		float theta1 = PI * (time / 1000.0 + vPosition.x / period1);
 		float theta2 = PI * (time / 1000.0 + vPosition.z / period2);
-		vModulated.y = vModulated.y + magnitude1 * sin(theta1) + magnitude2 * cos(theta2);
+		
+		//make "origin" of periodic floor waves dance back and forth
+		int itime = int(time) % (2*cyclelength);
+		itime = abs(itime - cyclelength);		
+			
+		float t = float(itime);
+		vModulated.y = vModulated.y + magnitude1 * sin(theta1) * (vModulated.x - 10.0 * (t - cyclelength/2.0) / cyclelength) + magnitude2 * cos(theta2) * (vModulated.z - 10.0 * (t - cyclelength/2.0) / cyclelength);
 	}
     gl_Position = Projection * ModelView * ModelOrientation * vModulated;
     fPosition = ModelView * ModelOrientation * vModulated;
